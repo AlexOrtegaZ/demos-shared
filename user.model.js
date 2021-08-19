f80ad933eebd45b84c45dd2c164c66144d88670c
Julian Alejandro Ortega Zepeda <jaortega@tiempodevelopment.com>
2021-08-18 21:20:18 -0700

    First commit

---
+++ user.model.js
@@ -0,0 +1,13 @@
+class User {
+  constructor() {
+    this.userId = '';
+    this.name = '';
+    this.phoneNumber = '';
+    this.profilePictureUrl = '';
+    this.cognitoId = '';
+    this.createdAt = undefined;
+    this.updatedAt = undefined;
+  }
+}
+
+module.exports = User;
