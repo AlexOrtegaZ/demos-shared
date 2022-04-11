3d2f26f14ddd57995fcb51efc0642e204dd97944
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-04-10 22:31:51 -0500

    create manifesto comment

--- repositories/manifesto-comment.repository.js
+++ repositories/manifesto-comment.repository.js
@@ -1,12 +1,55 @@
+/*
+  DEMOS
+  Copyright (C) 2022 Julian Alejandro Ortega Zepeda, Erik Ivanov Domínguez Rivera, Luis Ángel Meza Acosta
+  This file is part of DEMOS.
+
+  DEMOS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DEMOS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const DbHelper = require("./db.helper");
+const ManifestoComment = require("../models/manifesto-comment.model");
 
 class ManifestoCommentRepository extends DbHelper {
-    constructor() {
-        super();
-        this.entityName = Member.name;
-        this.tableName = 'manifesto_comment';
-        this.colId = 'manifesto_comment_id';
-    }
+  constructor() {
+    super();
+    this.entityName = Member.name;
+    this.tableName = "manifesto_comment";
+    this.colId = "manifesto_comment_id";
+  }
+
+  /**
+   * Create manifesto comment
+   * @param {string} content
+   * @param {string} manifestoCommentParentId
+   * @param {string} memberId
+   * @param {string} manifesto_id
+   * @returns {Promise<ManifestoComment>}
+   */
+  async createManifestoComment(
+    content,
+    manifestoCommentParentId,
+    memberId,
+    manifestoId
+  ) {
+    const newComment = new ManifestoComment();
+    newComment.content = content;
+    newComment.manifestoCommentParentId = manifestoCommentParentId;
+    newComment.createdByMember = memberId;
+    newComment.manifestoId = manifestoId;
+
+    return this.create(newComment);
+  }
 }
 
 module.exports = new ManifestoCommentRepository();
